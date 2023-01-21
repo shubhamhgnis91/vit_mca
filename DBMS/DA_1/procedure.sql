93bbaa725eae013f6fc84360fcbea1cf8fd56e02
shubhamhgnis91 <shubhamhgnis91@gmail.com>
2023-01-21 08:42:09 +0000

    Update procedure.sql

--- DBMS/DA_1/procedure.sql
+++ DBMS/DA_1/procedure.sql
@@ -31,7 +31,30 @@ BEGIN
 INSERT INTO Product VALUES (p_code, p_name, p_price, p_mfg_date);
 END;
 /
-
-
 execute add_product('P101', 'Samsung Galaxy S500', 35000, '20-feb-2022');
 
+
+
+DECLARE 
+   num number; 
+   factorial number;  
+   
+FUNCTION fact(x number) 
+RETURN number  
+IS 
+   f number; 
+BEGIN 
+   IF x=0 THEN 
+      f := 1; 
+   ELSE 
+      f := x * fact(x-1); 
+   END IF; 
+RETURN f; 
+END;  
+
+BEGIN 
+   num:= 6; 
+   factorial := fact(num); 
+   dbms_output.put_line(' Factorial '|| num || ' is ' || factorial); 
+END; 
+/
