230849fd3a60b7c7b9c5669edc1d58dc1c80fda2
shubhamhgnis91 <shubhamhgnis91@gmail.com>
2023-03-02 07:23:11 +0000

    Upload New File

---
+++ PYTHON/2.py
@@ -0,0 +1,25 @@
+day=int(input())
+month=int(input())
+year=int(input())
+cYear=int(input())
+
+if cYear<year : 
+    print("Invalid Input")
+    
+else : 
+    
+    nBdays=cYear-year
+    
+    if day != 29 and month != 2:
+        print(nBdays)
+        
+        
+    else:
+        nBdays=0
+        
+        for i in range(year,cYear+1,4):
+            if (i%4==0 and i%100!=0) or i%400==0:
+                nBdays=nBdays + 1
+        
+        print(nBdays)
+            
