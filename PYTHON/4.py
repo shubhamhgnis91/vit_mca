68dcc457b8b6b19375cb4f26b7ccff7e1a6e1bc7
shubhamhgnis91 <shubhamhgnis91@gmail.com>
2023-03-15 05:33:10 +0000

    Delete 4.py

--- PYTHON/4.py
+++
@@ -1,7 +0,0 @@
-inAmount=int(input())
-nYears=int(input())
-exAmount=int(input())
-
-interestRate=(((exAmount-inAmount)/(inAmount*nYears))*100)
-
-print("Required rate of interest is: " +str(interestRate))
